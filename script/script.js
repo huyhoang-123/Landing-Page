2add5861931245d6660ed00d94c0f501efbc721b
huy hoang <vuhuyhoang.st@gmail.com>
2025-08-18 17:19:16 +0700

    update interface

--- script/script.js
+++ script/script.js
@@ -2,3 +2,8 @@ const navigate =(url)=>{
 
     window.location.href = url;
 };
+
+const toggleMenu = () => {
+    const menu = document.querySelector('.cover-img');
+    menu.classList.toggle('active');
+}
